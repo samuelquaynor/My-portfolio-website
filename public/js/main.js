33b9f60c1bf07a1dd40e390dfdb6e5c9433c5bea
Samuel Williams <samwillsquaye@gmail.com>
2023-07-25 15:43:17 +0000

    links fix

--- public/js/main.js
+++ public/js/main.js
@@ -38,61 +38,61 @@ import Swiper from "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.brow
     }
   };
 
-  let portfolioContainer = select(".portfolio-container");
-  if (portfolioContainer) {
-    let portfolioIsotope = new Isotope(portfolioContainer, {
-      itemSelector: ".portfolio-item",
-      layoutMode: "fitRows",
-    });
+  // let portfolioContainer = select(".portfolio-container");
+  // if (portfolioContainer) {
+  //   let portfolioIsotope = new Isotope(portfolioContainer, {
+  //     itemSelector: ".portfolio-item",
+  //     layoutMode: "fitRows",
+  //   });
 
-    let portfolioFilters = select("#portfolio-flters li", true);
+  //   let portfolioFilters = select("#portfolio-flters li", true);
 
-    on(
-      "click",
-      "#portfolio-flters li",
-      function (e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function (el) {
-          el.classList.remove("filter-active");
-        });
-        this.classList.add("filter-active");
+  //   on(
+  //     "click",
+  //     "#portfolio-flters li",
+  //     function (e) {
+  //       e.preventDefault();
+  //       portfolioFilters.forEach(function (el) {
+  //         el.classList.remove("filter-active");
+  //       });
+  //       this.classList.add("filter-active");
 
-        portfolioIsotope.arrange({
-          filter: this.getAttribute("data-filter"),
-        });
-      },
-      true
-    );
-  }
+  //       portfolioIsotope.arrange({
+  //         filter: this.getAttribute("data-filter"),
+  //       });
+  //     },
+  //     true
+  //   );
+  // }
 
-  // window.addEventListener("DOMContentLoaded", () => {
-  //   let portfolioContainer = select(".portfolio-container");
-  //   if (portfolioContainer) {
-  //     let portfolioIsotope = new Isotope(portfolioContainer, {
-  //       itemSelector: ".portfolio-item",
-  //       layoutMode: "fitRows",
-  //     });
+  window.addEventListener("DOMContentLoaded", () => {
+    let portfolioContainer = select(".portfolio-container");
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: ".portfolio-item",
+        layoutMode: "fitRows",
+      });
 
-  //     let portfolioFilters = select("#portfolio-flters li", true);
+      let portfolioFilters = select("#portfolio-flters li", true);
 
-  //     on(
-  //       "click",
-  //       "#portfolio-flters li",
-  //       function (e) {
-  //         e.preventDefault();
-  //         portfolioFilters.forEach(function (el) {
-  //           el.classList.remove("filter-active");
-  //         });
-  //         this.classList.add("filter-active");
+      on(
+        "click",
+        "#portfolio-flters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
 
-  //         portfolioIsotope.arrange({
-  //           filter: this.getAttribute("data-filter"),
-  //         });
-  //       },
-  //       true
-  //     );
-  //   }
-  // });
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+        },
+        true
+      );
+    }
+  });
 
   /**
    * Scrolls to an element with header offset
